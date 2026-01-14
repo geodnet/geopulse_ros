0c6499293f77ce75e4115977e047f36c717620de
Arunabh Sharma <arunabh.1990@gmail.com>
2026-01-13 16:07:55 -0800

    add setup.py instead of pyproject since ROS2 doesn't support pyproject yet, remove additional unused nodes and other minor fixes

---
+++ src/nmea_navsat_driver/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = "nmea_navsat_driver"
+
+setup(
+    name=package_name,
+    version="2.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Nithya Srinivasan",
+    maintainer_email="nithya@geodnet.com",
+    description="Package to parse NMEA strings and publish a very simple GPS message.",
+    license="BSD",
+    entry_points={
+        "console_scripts": [
+            "nmea_serial_driver = libnmea_navsat_driver.nodes.nmea_serial_driver:main",
+            "nmea_topic_driver = libnmea_navsat_driver.nodes.nmea_topic_driver:main",
+            "nmea_topic_serial_reader = libnmea_navsat_driver.nodes.nmea_topic_serial_reader:main",
+        ],
+    },
+)
